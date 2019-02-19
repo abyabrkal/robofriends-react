a6e7e32287ad15d424e5a29de1ff8321d73e2fb2
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-02-19 11:25:44 +0400

    Propped ID. The image is randomised with id value using template strings.

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className="tc bg-light-blue dib pa3 ma2 grow bw2 shadow-5">
-            <img alt='profile' src='https://robohash.org/test?200x200' />
+            <img alt='profile' src={`https://robohash.org/${props.id}?200x200`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
