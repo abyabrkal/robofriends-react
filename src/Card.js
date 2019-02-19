fa05d445c8da04fd1dd2390cda639221daded841
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-02-19 11:26:45 +0400

    Destructuring props right within parameter.

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Card = (props) => {
+const Card = ({ id, name, email }) => {
     return (
         <div className="tc bg-light-blue dib pa3 ma2 grow bw2 shadow-5">
-            <img alt='profile' src={`https://robohash.org/${props.id}?200x200`} />
+            <img alt='profile' src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
