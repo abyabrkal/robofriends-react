9aacd10a011c182f450e56ce9b26765c5d576269
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-02-20 10:21:03 +0400

    Filtered the data based on search box value. Misterious error on toLowercase removed by copy code from git original.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots = { robots } />
-        </div>
-        
-    );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+            
+        );
+    }
+    
 };
 
 export default App;
